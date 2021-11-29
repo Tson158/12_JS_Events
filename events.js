3f86c940c3860538338c933f04b65edd9f2c4b1e
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-11-29 07:56:32 +0100

    Add files via upload

---
+++ events.js
@@ -0,0 +1,21 @@
+
+/********  Variablen **********/
+
+
+
+/********  Event-Listener **********/
+
+
+
+/********  Business-Logic | Toggle **********/
+
+
+
+/********  Änderung in View-Schicht **********/
+
+/
+
+/* Tools */
+function output(outputData) {
+    console.log(outputData);
+}
