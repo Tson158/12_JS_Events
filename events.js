e409d9d253f61bbe18afcb60ac0e47433790dfa6
Kakarott <Sonntag_thomas_86@web.de>
2021-11-29 12:22:09 +0100

    event ++

--- events.js
+++ events.js
@@ -1,17 +1,35 @@
 
 /********  Variablen **********/
 
-const btn = document.getElementById("trigBtn");
+// const btn = document.getElementById("trigBtn");
+
+const btn = document.body.children[0];
+
+
 // output(btn);
 
 
 /********  Event-Listener **********/
 
-btn.addEventListener("click",test);
+btn.addEventListener("click",onClick);
+btn.addEventListener("mouseenter", onRollIn);
+btn.addEventListener("mouseleave", onRollOut);
 
-function test() {
+function onClick() {
     output("hey");
 }
+
+function onRollIn() {
+    output("1");
+}
+
+function onRollOut() {
+    output("2");
+}
+
+
+
+
 
 /********  Business-Logic | Toggle **********/
 
